ee91873f60da0fbb0c0d168cd7379d59d43f09f8
leoheinhtet <ronin.kamikaze22@gmail.com>
2025-06-20 15:00:32 +0630

    printingout receipt and writing sales record to sales.csv

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from inventory import Inventory
 from item import Item
 from cart import Cart
+from datetime import datetime
+import csv
+
+def generate_receipt_id(counter_file="receipt_counter.txt"):
+    try:
+        with open(counter_file, "r") as file:
+            last_id = int(file.read().strip())
+    except FileNotFoundError:
+        last_id = 0
+
+    new_id = last_id + 1
+
+    with open(counter_file, "w") as file:
+        file.write(str(new_id))
+
+    return f"R{new_id:04d}"
+
 
 def main():
     inventory = Inventory("inventory.csv")
@@ -44,15 +61,32 @@ def main():
                     print("Your cart is empty. Please add items before checking out.")
                 else:
                     print("Checking out...")
-                
+                    receipt_id = generate_receipt_id()
+                    time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                         
+                    try:
+                        with open("sales.csv", mode="a", newline='', encoding='utf-8') as file:
+                            writer = csv.writer(file)
+                            #write header if file is empty
+                            if file.tell() == 0:
+                                writer.writerow(["Receipt_ID","Item_ID", "Item_Name", "Quantity", "Unit_Price", "Total_Price", "Timestamp"])
+                            for item, quantity in cart.get_items():
+                                 total_price = item.price * quantity
+                                 inventory.update_stock(item.id, quantity)
+                                 # Write to sales.csv
+                                 writer.writerow([receipt_id, item.id, item.name, quantity, item.price, total_price, time_stamp])
+                                 print(f"Item: {item.name}, Quantity: {quantity}, Unit Price: ${item.price:.2f}, Total Price: ${total_price:.2f}")
+
+                        print(f"Receipt ID: {receipt_id}")
+                        print(f"Total Amount: ${cart.total_price():.2f}")
+                        print("Thank you for your purchase!")
+                        cart.clear()
+                        inventory.save()
+                    except Exception as e:
+                        print(f"An error occurred during checkout: {e}")
                     
-                    for item, quantity in cart.get_items():
-                        inventory.update_stock(item.id, quantity)
-                    print(f"Total price: ${cart.total_price():.2f}")
-                    cart.clear()
-                    inventory.save() # update inventory.csv after checkout
-                    print("Thank you for your purchase!")
+                                 
+                    
                         
                     
             
